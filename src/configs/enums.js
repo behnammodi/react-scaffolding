b9d826483c60726b9b20004d25bc53e7675b7fa2
Behnam Mohammadi (بهنام محمدی) <itten@live.com>
2019-03-25 12:16:11 +0430

    moved enums to configs

---
+++ src/configs/enums.js
@@ -0,0 +1,5 @@
+export const SHOULD_NOT_AUTH = 1;
+export const SHOULD_AUTH = 2;
+export const NO_DIFFERENT = 3;
+export const APP_ENV_PRODUCTION = 'production';
+export const APP_ENV_DEVELOPMENT = 'development';
